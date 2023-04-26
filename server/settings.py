3fa91d7c550e00e0427683f2d0f922f060d61fc9
marvinoka4 <marvinoka4@gmail.com>
2023-04-26 21:54:09 +0100

    add staticfiles

--- server/settings.py
+++ server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'core.apps.CoreConfig',
     'blog.apps.BlogConfig',
 ]
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
