9e3828e4b514adad80db00fe2e2c9259af779bd6
marvinoka4 <marvinoka4@gmail.com>
2023-04-27 02:57:39 +0100

    add database

--- server/settings.py
+++ server/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-w#n+ogc!i2nab7$+-ui)-52yn$(d9g_u77b**#=v_)4hdceib-
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://tech-in-energy.azurewebsites.net']
+ALLOWED_HOSTS = ['tech-in-energy.azurewebsites.net']
+# ALLOWED_HOSTS = [ ]
 
 
 # Application definition
@@ -81,9 +82,14 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "techenergydb",
+        "USER": "dragonlord",
+        "PASSWORD": "DragonLord20!",
+        "HOST": "tech-energy-db.postgres.database.azure.com",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
